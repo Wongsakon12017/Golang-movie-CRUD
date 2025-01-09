1b51e18263f569db1b759a548d690a5a39c09e92
Wongsakon Kwangin <birdzer12017@gmail.com>
2025-01-10 00:50:24 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Wongsakon12017
+
+go 1.23.4
+
+require github.com/gorilla/mux v1.8.1 // indirect
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+}
+
+func main() {
+
+}
